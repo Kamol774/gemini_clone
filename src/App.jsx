a20cb6b8c9fe0af370d7c1401f2ac69699851775
Kamol774 <chust1222@gmail.com>
2024-12-01 12:12:40 +0900

    feat: add sidebar, create main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React  from "react";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Main from "./components/Main/Main";
 
 const App = () => {
   return (
-    <div></div>
+    <>
+    <Sidebar/>
+    <Main/>
+    </>
   )
 }
 
