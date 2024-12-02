d9e8139f8393ba36f71b7c8d2e73351823404e22
Kamol774 <chust1222@gmail.com>
2024-12-02 23:05:45 +0900

    fix: sidebar fix

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -11,14 +11,72 @@ const ContextProvider = (props) => {
     const [recentPrompt, setRecentPrompt] = useState("")
     const [prevPrompts, setPrevPrompts] = useState([])
     const [showResult, setShowResult]=useState(false)
-    
+    const [loading, setLoading] = useState(false)
+    const [resultData, setResultData] = useState("")
 
+    const delayPara = (index, nextword) => {
+        setTimeout(function () {
+            setResultData(prev=>prev+nextword)
+        }, 75*index)
+    }
+    
+    const newChat = () => {
+        setLoading(false)
+        setShowResult(false)
+    }
+    
     const onSent = async (prompt) => {
-        await run(prompt)
+        setInput("")
+        setResultData("")
+        setLoading(true)
+        setShowResult(true)
+        let response;
+        if (prompt !== undefined) {
+            response = await run(prompt)
+            setRecentPrompt(prompt)
+        }
+        else 
+        {
+            setPrevPrompts(prev=>[...prev, input])
+            setRecentPrompt(input)
+            response = await run(input)
+        }
+
+        let responseArray = response.split("**")
+        let newResponse="";
+        for(let i=0; i<responseArray.length; i++)
+        {
+            if (i === 0 || i%2 !== 1) {
+                newResponse += responseArray[i]
+            } 
+            else {
+                newResponse += "<b>"+responseArray[i]+"</b>"
+            }
+        }
+        let newResponse2 = newResponse.split("*").join("<br />")
+        let newResponseArray = newResponse2.split(" ")
+        for(let i=0; i<newResponseArray.length; i++)
+        {
+            const nextWord = newResponseArray[i]
+            delayPara(i, nextWord+" ")
+        }
+
+        setLoading(false)
+
     }
 
+    
     const contextValue = {
-
+        prevPrompts,
+        setPrevPrompts,
+        onSent,
+        setRecentPrompt,
+        recentPrompt,
+        showResult,
+        loading,
+        resultData,
+        setInput,
+        newChat
     }
 
     return (
