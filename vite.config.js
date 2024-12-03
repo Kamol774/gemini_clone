7eac995541d91c318144ebf95c62e30b012f193f
Kamol774 <chust1222@gmail.com>
2024-12-03 23:27:10 +0900

    modified vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '194.164.77.212',  // Listen on all network interfaces (useful for accessing from other devices)
+    port: 6000,       // Set the desired port number
+  }
+});
